fea2d6b82116590b380ff476279fd3904a5df710
Sean Welleck <wellecks@gmail.com>
2024-04-02 01:45:19 -0400

    Update prepare_data.sh

--- partI_nextstep/ntp-tune/prepare_data.sh
+++ partI_nextstep/ntp-tune/prepare_data.sh
@@ -1,4 +1,4 @@
 
 mkdir data
-wget -O data/state_tactic_mathlib_only_train.jsonl https://huggingface.co/datasets/wellecks/ntp-lean-mathlib-tactic-instruct/blob/main/state_tactic/state_tactic_mathlib_only_train.jsonl
-wget -O data/state_tactic_mathlib_only_dev.jsonl https://huggingface.co/datasets/wellecks/ntp-lean-mathlib-tactic-instruct/blob/main/state_tactic/state_tactic_mathlib_only_dev.jsonl
+wget -O data/state_tactic_mathlib_only_train.jsonl https://huggingface.co/datasets/l3lab/ntp-mathlib-instruct-st/blob/main/state_tactic_mathlib_only/state_tactic_train.jsonl
+wget -O data/state_tactic_mathlib_only_dev.jsonl https://huggingface.co/datasets/l3lab/ntp-mathlib-instruct-st/blob/main/state_tactic_mathlib_only/state_tactic_dev.jsonl
